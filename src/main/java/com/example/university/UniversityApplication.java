41dd6c44f8faef5d41367e88eac5f319dc8cfbee
lucas <lucasbalon@gmail.com>
2023-11-07 16:04:46 +0100

    Add TODO comment in UniversityApplication class
    This commit adds a TODO comment in the UniversityApplication class to remind developers to implement PreAuthorize and DTOs in the controllers. These improvements are necessary for stronger access control and data transfer management in subsequent revisions.

--- src/main/java/com/example/university/UniversityApplication.java
+++ src/main/java/com/example/university/UniversityApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniversityApplication {
-
+    //TODO: Insérer les PreAuthorize, utiliser les DTO dans les controllers
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
     }
-
 }
